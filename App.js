a867d89aeba32827abbb145db3e46930e5e19cc2
mihneagaleteanu <ahcasa@gmail.com>
2019-10-08 07:43:17 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import { Alert,Button, View, Text, Linking } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; 
+import TopUp from './components/TopUp';
+
+class HomeScreenBrowser extends React.Component {
+
+  navigate() {
+    this.props.navigation.navigate('TopUp',{
+      memberId: "12345",
+      balance: 5000,
+      email:"mihnea@points.com",
+      firstName:"Mihnea",
+      lastName:"Galeteanu",
+      transactionQuantity: 3000
+    });
+  }
+
+  openBrowser() {
+    Linking.openURL("https://buymiles.mileageplus.com/united/united_landing_page/");
+  }
+
+  render() {
+    Alert.alert('','Your account does not have enough miles to complete the transaction. Please purchase miles or search by price.',[
+    {text: 'PURCHASE MILES',onPress: () => this.openBrowser()},{text: 'SEARCH BY PRICE'}
+  ]);
+  return null;
+  }
+}
+
+class HomeScreenWebView extends React.Component {
+
+  navigate() {
+    this.props.navigation.navigate('TopUp',{
+      memberId: "12345",
+      balance: 5000,
+      email:"mihnea@points.com",
+      firstName:"Mihnea",
+      lastName:"Galeteanu",
+      transactionQuantity: 3000
+    });
+  }
+
+  render() {
+    Alert.alert('','Your account does not have enough miles to complete the transaction. Please purchase miles or search by price.',[
+    {text: 'PURCHASE MILES',onPress: () => this.navigate()},{text: 'SEARCH BY PRICE'}
+  ]);
+  return null;
+  }
+}
+
+class HomeScreenOld extends React.Component {
+  render() {
+    Alert.alert('','Your account does not have enough miles to complete the transaction. Please search by price or go to buymiles.mileageplus.com to purchase or transfer miles.',[
+    {text: 'CONTINUE'}
+  ]);
+  return null;
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreenOld,
+    TopUp: TopUp,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Top Up Example App
+
+Welcome to the Points Top Up Example!
+
+
